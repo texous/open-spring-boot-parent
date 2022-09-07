03e6957c15345e2a413b0ed9e0cdf57ff38b89c6
Showa.L <liuxh@4dstc.com>
2022-09-07 09:59:59 +0800

    完善 POM 结构，添加批量构建脚本

---
+++ cmd/README.md
@@ -0,0 +1,3 @@
+# script 使用帮助
+
+使用前请先配置好 `JAVA_HOME_8` 和 `JAVA_HOME_17` 环境变量，分别对应 `jdk8` 和 `jdk17`
---
+++ cmd/mvn-17-deploy.bat
@@ -0,0 +1,29 @@
+@echo off
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: 获取脚本文件名
+set file_name=%0
+
+for /f "tokens=2 delims=-" %%a in ("%file_name%") do (
+	::输出第一个分段(令牌)
+	set java_version=%%a
+)
+
+set "java_home_str=JAVA_HOME_%java_version%"
+
+if "%java_version%" == "" (
+    set java_home_str=JAVA_HOME
+)
+
+echo ---- JAVA_HOME name: %java_home_str%
+
+set JAVA_HOME=!%java_home_str%!
+
+set "profile=-Pjdk%java_version%"
+
+echo ---- JAVA_HOME: %JAVA_HOME%
+echo ---- PROFILE: %profile%
+
+cd ../
+mvn clean deploy %profile%
